2818f65fa5af4c3a61d4f685addf87b5a2327ed7
Maximiliano Trochon Canto <maxitc8@hotmail.com>
2023-10-22 00:12:24 -0300

    primer commit

--- src/paginas/Calzado.jsx
+++ src/paginas/Calzado.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ItemListContainer from '../componentes/itemList/itemListContainer'
 
 export const Calzado = () => {
-  return <p>Puta</p>
-    // <ItemListContainer categoria={"Calzado"} greetings={"Bienvenidos!"}/>
+  return <ItemListContainer categoria={"Calzado"} greetings={"Bienvenidos!"}/>
   
 }
