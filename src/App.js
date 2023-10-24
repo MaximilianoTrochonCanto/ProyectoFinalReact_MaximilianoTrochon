175f72ce4890e62c8a5872cdbc6e9e34a5f20928
Maximiliano Trochon Canto <maxitc8@hotmail.com>
2023-10-23 22:48:23 -0300

    cuarto commit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ return(
         <NavbarBootstrap/>
       </header>
       <Routes>
+        <Route path="/" element={<Remeras/>} />                                    
         <Route path="/ProyectoFinalReact_MaximilianoTrochon/" element={<Remeras/>} />                                    
         <Route path="/ProyectoFinalReact_MaximilianoTrochon/cart" element={<Cart/>} />                    
         <Route path="/ProyectoFinalReact_MaximilianoTrochon/checkout" element={<Checkout/>} />            
